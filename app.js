848d0ba965cb9373e09fcc61c4ea3821c705d94c
Cristian Espiritu <cxistian@gmail.com>
2017-12-20 11:00:03 -0600

    Ruta de consulta por querystring agregada

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ var path = require('path')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-var sql = require('mssql')
-
-var routes = require('./routes')
 var index = require('./routes/index')
-var consultas = require('./routes/ConsultaLabCore')
+var consulta = require('./routes/ConsultaLabCore')
 
 var app = express()
-app.use('/', routes)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -25,7 +21,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/ConsultaLabCore', consultas)
+app.use('/ConsultaLabCore', consulta)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/ConsultaLabCore.js
+++ routes/ConsultaLabCore.js
@@ -1,8 +1,27 @@
 var express = require('express')
 var router = express.Router()
+var sql = require('mssql')
+// Conexión a la BD LABCORE
+const DbConnectionString = 'mssql://labcore:labcore@192.168.1.10:1433/LabCore'
+
+// error Handler
+sql.on('error', err => {
+  console.dir(err)
+  sql.close()
+})
 
 /* GET consulta. */
-express.Router('/ConsultaLabCore').get(function (req, res, next) {
-  res.send('Regresara el JSON')
+router.get('/', function (req, res, next) {
+  sql.connect(DbConnectionString).then(pool => {
+    return pool.request()
+      .query(req.query.consulta)
+  }).then(result => {
+    sql.close()
+    res.send(JSON.stringify(result.recordset))
+  }).catch(err => {
+    console.dir(err)
+    sql.close()
+  })
 })
+
 module.exports = router
