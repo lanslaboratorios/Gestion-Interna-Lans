75e704477ecab13658f5962fb241af23510f7ff9
Cristian Espiritu <cxistian@gmail.com>
2018-01-05 17:44:30 -0600

    cors

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 app.use('/ConsultaLabCore', consulta)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
