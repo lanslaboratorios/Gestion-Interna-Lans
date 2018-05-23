ce5b47f8699121f2663f6b5565cf737b94c50ed1
Cxistian <cxistian@gmail.com>
2018-05-23 11:22:24 -0500

    [ADD] endpoint auth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('Gestion Interna Labcore V 1.0.0')
-})
+router.get('/', function (req, res){
+  res.send('Gestion Interna Labcore V 1.0.0');
+});
 
-module.exports = router
+module.exports = router;
---
+++ routes/isBlocked.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var router = express.Router()
+var sql = require('mssql')
+// Conexión a la BD LABCORE
+const DbConnectionString = 'mssql://labcore:labcore@192.168.1.10:1433/LabCore'
+// error Handler
+sql.on('error', err => {
+  console.dir(err)
+  sql.close()
+})
+
+/* GET consulta. */
+router.get('/', function (req, res) {
+  sql.connect(DbConnectionString).then(pool => {
+    return pool.request()
+      .query(`select  Estatus_del_Socio from DatosSocios_Portales_View where ID_Socio = ${req.query.id}`)
+      
+  }).then(result => {
+    sql.close()
+    // res.json(result);
+     if( result.rowsAffected == 0){
+     res.send('-1');}
+     else if(result.rowsAffected >1){
+        res.send('-2'); 
+     }else if( result.recordset[0].Estatus_del_Socio == 'Activo'){
+        res.send('1');
+    }else{
+        res.send('0');
+    }
+  }).catch(err => {
+    console.dir(err)
+    sql.close()
+  })
+})
+
+router.post('/', function (req, res) {
+    sql.connect(DbConnectionString).then(pool => {
+      return pool.request()
+        .query(`select  Estatus_del_Socio from DatosSocios_Portales_View where ID_Socio = ${req.body.id}`)
+        
+    }).then(result => {
+      sql.close() 
+        if( result.rowsAffected == 0){
+        res.send('-1');}
+        else if(result.rowsAffected >1){
+           res.send('-2'); 
+        }else if( result.recordset[0].Estatus_del_Socio == 'Activo'){
+           res.send('1');
+       }else{
+           res.send('0');
+       }
+    }).catch(err => {
+      console.dir(err)
+      sql.close()
+    })
+  })
+
+module.exports = router
